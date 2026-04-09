2b377931800e16d424efe5814f1bf7c3ff636310
perceval <sylvain.de.luca@gmail.com>
2026-04-09 15:01:41 +0200

    prisma change

--- app/[domain]/lib/prisma.ts
+++ app/[domain]/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../../generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 // Extend the global object to include the PrismaClient instance
 declare global {
@@ -6,6 +6,7 @@ declare global {
 }
 
 export const prisma = globalThis.prisma || new PrismaClient();
+
 // Ensure the PrismaClient instance is not recreated in development
 if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
 
